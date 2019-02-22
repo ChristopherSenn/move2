55b04618102b702293f6717e00da297d4d2e12c6
chris-PC\chris <christopher@senn-b.de>
2019-02-22 15:29:29 +0100

    filter fix

--- src/app/modules/study-abroad/pages/map/map.component.ts
+++ src/app/modules/study-abroad/pages/map/map.component.ts
@@ -38,17 +38,18 @@ export class MapComponent implements OnInit {
 
   private onFilterChanged(universityFilters: UniversityFilters[]) {
     this.universityFilters = universityFilters;
-
+    //creates an array that contains all the IDs of the filters that are true (trues[category][filter] = id)
     let trues: number[][] = [];
     for (let cats = 0; cats < universityFilters.length; cats++) {
       trues[cats] = [];
       for (let i = 0; i < universityFilters[cats].content.length; i++) {
         if(universityFilters[cats].content[i].isActive){
           trues[cats].push(universityFilters[cats].content[i].id);
-          console.log(universityFilters[cats].content[i].id);
         }
       }
     }
+    //creates an array that contains all the IDs of the universities that fulfill each filter results[filterCategory][idOfUniversities] 
+    //=> contains duplicates, because each active filter can be fullfilled from multiple universities
     let results: number [][] = [];
     for (let cats = 0; cats < universityFilters.length; cats++) {
       results[cats] = [];
@@ -60,26 +61,60 @@ export class MapComponent implements OnInit {
         }
       }
     }
-
+    //deletes all duplicates from each category
+    //=> results in for example: language can be englich OR german if both are activated
     for (let cats = 0; cats < results.length; cats++) {
       results[cats] = results[cats].filter((el, i, a) => i === a.indexOf(el))
     }
-    
-    this.displayableUniversities = [];
+    //if in a category no filter is activated, it is removed 
+    //=> new array with only the relevant categorys
+    let relevantResults = [];
     for (let cats = 0; cats < results.length; cats++) {
-      this.displayableUniversities = this.displayableUniversities.concat(results[cats]);
-    }
-    
-    this.displayableUniversities = this.displayableUniversities.filter((el, i, a) => i === a.indexOf(el));
-
-    this.filterIsActive = false;
-    for (let cats = 0; cats < trues.length; cats++) {
-      if (trues[cats].length != 0) {
-        this.filterIsActive = true;
-        break;
+      if (results[cats].length != 0) {
+        relevantResults.push(results[cats]);
       }
     }
-    
+    //adds the results up in a single array.
+    let relevantResults2: number [][] = []
+    for (let cats = 0; cats < relevantResults.length; cats++) {
+      relevantResults2 = relevantResults2.concat(relevantResults[cats]);
+    }
+    //counts how often each universityID occurs in the combined array
+    // a[]: universityIDs - b[]: how often does the ID occur
+    var a = [], b = [], prev;
+
+    relevantResults2.sort();
+    for ( var i = 0; i < relevantResults2.length; i++ ) {
+        if ( relevantResults2[i] !== prev ) {
+            a.push(relevantResults2[i]);
+            b.push(1);
+        } else {
+            b[b.length-1]++;
+        }
+        prev = relevantResults2[i];
+    }
+    //if an ID occurs less than the count of activated Filter Categorys, it is marked
+    //This results in th following: an university has to have the languages (English OR German) AND the faculty has to be (Ei OR M)
+    for (let i = 0; i < b.length; i++) {
+      if (b[i] != relevantResults.length) {
+        a[i] = -1;
+      }
+    }
+    //all the unmarked (relevant/filtered) universityIDs are saved in the final array that is passed to the map component
+    this.displayableUniversities = [];
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] != -1) {
+        this.displayableUniversities.push(a[i]);
+      } 
+    }
+    //determines if any filter is activated. This is neccessary because the final Array is empty if there are no filters at all AND if there are no fitting universities
+    //the boolean tells the map component if it hast to apply the empty array or not
+    if (this.displayableUniversities.length == 0) {
+      this.filterIsActive = false;
+    } else {
+      this.filterIsActive = true;
+    }
+
   }
 
 }
